ec57f8885d619557709995b5513bada75010a23c
Shrimp <Shrimp@ShrimProd>
2016-10-28 18:45:07 +0300

    Stack on beautifying listviews

--- Parser/Form1.Designer.cs
+++ Parser/Form1.Designer.cs
@@ -28,18 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Item1");
-            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("Item2");
-            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("Item3");
-            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("Item4");
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Item1");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Item2");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Item3");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("Item4");
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.lv_british = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -57,6 +58,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(623, 34);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "effect";
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // button1
@@ -74,7 +76,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 29;
-            this.listBox1.Location = new System.Drawing.Point(33, 35);
+            this.listBox1.Location = new System.Drawing.Point(151, 156);
             this.listBox1.Margin = new System.Windows.Forms.Padding(5);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(293, 236);
@@ -84,46 +86,45 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(14, 116);
+            this.tabControl1.Location = new System.Drawing.Point(14, 75);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(817, 470);
+            this.tabControl1.Size = new System.Drawing.Size(817, 511);
             this.tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.lv_british);
             this.tabPage1.Controls.Add(this.listBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 38);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(809, 428);
+            this.tabPage1.Size = new System.Drawing.Size(809, 469);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "British";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // label1
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 38);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(802, 546);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "American";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
             // 
             // lv_british
             // 
             this.lv_british.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
             this.lv_british.FullRowSelect = true;
-            this.lv_british.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.lv_british.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem5,
-            listViewItem6,
-            listViewItem7,
-            listViewItem8});
-            this.lv_british.Location = new System.Drawing.Point(362, 35);
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.lv_british.Location = new System.Drawing.Point(392, 50);
             this.lv_british.Name = "lv_british";
             this.lv_british.Size = new System.Drawing.Size(396, 479);
             this.lv_british.TabIndex = 3;
@@ -132,7 +133,17 @@
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Width = 390;
+            this.columnHeader1.Width = 255;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 38);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(809, 469);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "American";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // menuStrip1
             // 
@@ -157,7 +168,7 @@
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(108, 26);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // aboutToolStripMenuItem
@@ -171,7 +182,7 @@
             // aboutToolStripMenuItem1
             // 
             this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
-            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(181, 26);
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(125, 26);
             this.aboutToolStripMenuItem1.Text = "About";
             // 
             // Form1
@@ -192,6 +203,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -214,6 +226,7 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Parser/Form1.cs
+++ Parser/Form1.cs
@@ -29,18 +29,19 @@ namespace Parser
 
         private void button1_Click(object sender, EventArgs e)
         {
-            search(textBox1.Text, dic_type.British);
+            search(textBox1.Text);
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyValue == 13)
-                search(textBox1.Text, dic_type.British);
+                search(textBox1.Text);
         }
 
-        private void search(string q, dic_type dtype)
+        private void search(string q, dic_type dtype = dic_type.British)
         {
-            string dselector = ".//div[@data-tab='ds-british']";
+            // Dictionary choosing stuff
+            string dselector = ".//";
             switch (dtype)
             {
                 case dic_type.British:
@@ -52,6 +53,7 @@ namespace Parser
             }
             try
             {
+                // Receiving HTML-page stuff
                 System.Net.WebClient web = new System.Net.WebClient();
                 web.Encoding = UTF8Encoding.UTF8;
                 string html = web.DownloadString("http://dictionary.cambridge.org/search/english/direct/?q=" + System.Web.HttpUtility.UrlEncode(q));
@@ -59,26 +61,73 @@ namespace Parser
                 doc.LoadHtml(html);
 
 
+                // Parsing
                 List<string> defs = new List<string>();
                 HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(dselector)[0].SelectNodes(".//div[@class='entry-body__el clrd js-share-holder']");
                 if (nodes == null)
+                {
                     process_err("Error while parsing");
+                    return;
+                }
 
+                tabControl1.TabPages.Clear();
                 foreach (HtmlNode group in nodes)
                 {
-                    label1.Text = "Showing word: " + group.SelectNodes(".//span[@class='headword']")[0].InnerText;
-                    defs.Add("<hw>: " + group.SelectNodes(".//span[@class='headword']")[0].InnerText + "(" + group.SelectNodes(".//span[@class='posgram ico-bg']")[0].InnerText + ")");
+                    TabPage tp = new TabPage();
+                    //tp.Location = new System.Drawing.Point(4, 38);
+                    tp.Name = "tbpg_"+tabControl1.TabPages.Count;
+                    //tp.Padding = new System.Windows.Forms.Padding(3);
+                    //tp.Size = new System.Drawing.Size(809, 469);
+                    //tp.TabIndex = 0;
+                    tp.Text = group.SelectNodes(".//span[@class='headword']")[0].InnerText + " (" + group.SelectNodes(".//span[@class='posgram ico-bg']")[0].InnerText + ")";
+                    tp.UseVisualStyleBackColor = true;
+                    tabControl1.TabPages.Add(tp);
+
                     HtmlNodeCollection senseblocks = group.SelectNodes(".//div[@class='sense-block']");
+                    int last_block_y = 3;
                     foreach(HtmlNode senseblock in senseblocks)
                     {
                         HtmlNodeCollection difdef = senseblock.SelectNodes(".//h4[@class='txt-block txt-block--alt2']");
                         if (difdef != null && difdef.Count > 0)
-                            defs.Add(">>" + difdef[0].InnerText);
-                        HtmlNodeCollection defblock = senseblock.SelectNodes(".//div[@class='def-block pad-indent']");
-                        foreach(HtmlNode def in defblock)
                         {
-                            defs.Add(def.SelectNodes(".//b[@class='def']")[0].InnerText);
+                            Label sense_info = new Label();
+                            sense_info.AutoSize = true;
+                            sense_info.Location = new Point(6, last_block_y);
+                            sense_info.Name = "lbl_senseinfo_" + tp.Text + "_" + tabControl1.TabPages.Count;
+                            //sense_info.Size = new System.Drawing.Size(79, 29);
+                            //sense_info.TabIndex = 4;
+                            sense_info.Text = difdef[0].SelectNodes(".//span[@class='guideword']")[0].ChildNodes[1].InnerText;
+                            //sense_info.BackColor = Color.AliceBlue;
+                            tp.Controls.Add(sense_info);
+
+                            last_block_y = sense_info.Location.Y + sense_info.Height;
                         }
+                        
+                        HtmlNodeCollection defblocks = senseblock.SelectNodes(".//div[@class='def-block pad-indent']");
+                        if (defblocks == null)
+                        {
+                            process_err("Error while parsing");
+                            return;
+                        }
+
+                        ListView lv_def = new ListView();
+                        lv_def.Columns.Add(new ColumnHeader() );
+                        lv_def.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                        lv_def.FullRowSelect = true;
+                        lv_def.HeaderStyle = ColumnHeaderStyle.Clickable;
+                        lv_def.Location = new Point(0, last_block_y);
+                        lv_def.Name = "lv_" + tp.Text + "_" + tp.Controls.Count;
+                        lv_def.Size = new Size(396, 479);
+                        //lv_def.TabIndex = 3;
+                        //lv_def.UseCompatibleStateImageBehavior = false;
+                        lv_def.View = View.Details;
+
+                        foreach (HtmlNode def in defblocks)
+                        {
+                            lv_def.Items.Add(def.SelectNodes(".//b[@class='def']")[0].InnerText);
+                        }
+                        tp.Controls.Add(lv_def);
+                        last_block_y = lv_def.Location.Y + lv_def.Height;
                     }
                     //if (!defs.Contains(link.InnerText.Replace(":", "")))
                     //    defs.Add(link.InnerText.Replace(":", ""));
